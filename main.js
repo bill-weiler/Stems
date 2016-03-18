5ad1bce7094dffa659bf608d142fb5a6d60362e6
Student <Student@Mac-mini-2.local>
2016-03-18 16:53:17 -0600

    First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+angular.module('GreenSheets', [])
+.controller('GreenSheets', GreenSheets)
+
+// =======================================
+//              Controller As
+// =======================================
+  function GreenSheets = function(){
+    var g = GreenSheets
+  }
