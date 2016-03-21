5094a151aba2445a61ff39091b6cdaedfb5ad2fb
Student <Student@Mac-mini-2.local>
2016-03-21 12:16:51 -0600

    states initiated

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 (function() {
   'use strict';
-  angular.module('StemsAppp', ['GreenSheets'])
+  angular.module('StemsAppp', ['GreenSheets', 'ui.router'])
+  .config(MainRouter)
+
+//ui-router
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "home.html",
+    })
+    .state('clients', {
+      url: "/clients",
+      templateUrl: "clients.html",
+    })
+    .state('to-do', {
+      url: "/todo",
+      templateUrl: "to-do.html",
+    })
+  $urlRouterProvider.otherwise('/')
+}
 
 }());
--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 angular.module('GreenSheets', [])
-.controller('GreenSheets', GreenSheets)
+  .controller('MainController', MainController)
 
-// =======================================
-//              Controller As
-// =======================================
-  function GreenSheets = function(){
-    var g = GreenSheets
-  }
+//Controller As
+function MainController() {
+  var mainCtrl = this
+
+
+//==================VARIABLES===================\\
+
+//==Clients==\\
+mainCtrl.Smith = new Client('Sarah Smith', '555-555-5555', 'ssmith@gmail.com', '123 ABC Rd.', 'Denver', 'CO', '80218')
+mainCtrl.Jones = new Client('John Jones' , '555-555-5555', 'jjones@gmail.com', '456 DEF Rd.', 'Denver', 'CO', '80218')
+mainCtrl.Anderson = new Client('Alan Anderson' , '555-555-5555', 'aanderson@gmail.com', '789 GHI Rd.', 'Denver', 'CO', '80218')
+
+//==Client Array==\\
+var clientArray = [this.Smith, this.Jones, this.Anderson]
+
+//==========Client Constructor Function==========\\
+function Client(name, phoneNumber, email, address, city, state, zipCode) {
+this.name = name
+this.phoneNumber = phoneNumber
+this.email = email
+this.address = address
+this.city = city
+this.state = state
+this.zipCode = zipCode
+}
+
+} //End of controller
