dca95a2c04cfd4fb4a96d85b6704774cefc89cae
Student <Student@Mac-mini-2.local>
2016-03-21 16:12:39 -0600

    toDos ng-repeat complete

--- main.js
+++ main.js
@@ -10,36 +10,36 @@ function MainController($stateParams) {
 //==============================================\\
 
 //==Clients==\\
-mainCtrl.Smith = new Client('Sarah Smith', '555-555-5555', 'ssmith@gmail.com', '123 ABC Rd.', 'Denver', 'CO', '80218')
-mainCtrl.Jones = new Client('John Jones' , '555-555-5555', 'jjones@gmail.com', '456 DEF Rd.', 'Denver', 'CO', '80218')
-mainCtrl.Anderson = new Client('Alan Anderson' , '555-555-5555', 'aanderson@gmail.com', '789 GHI Rd.', 'Denver', 'CO', '80218')
+mainCtrl.Smith    = new Client('Sarah',  'Smith',    '555-555-5555', 'ssmith@gmail.com',    '123 ABC Rd.', 'Denver', 'CO', '80218')
+mainCtrl.Jones    = new Client('John',  'Jones' ,    '444-444-444',  'jjones@gmail.com',    '456 DEF Rd.', 'Denver', 'CO', '80218')
+mainCtrl.Anderson = new Client('Alan',  'Anderson' , '333-333-3333', 'aanderson@gmail.com', '789 GHI Rd.', 'Denver', 'CO', '80218')
 
 //==To-Dos==\\
-mainCtrl.toDoItem1 = new toDoItem('Sarah Smith', 'Spring clean-up and annuals')
-mainCtrl.toDoItem2 = new toDoItem('John Jones', 'Watering only')
-mainCtrl.toDoItem3 = new toDoItem('Alan Anderson', 'Irrigation and watering')
+mainCtrl.toDoItem1 = new toDoItem('Sarah',  'Smith',   'Spring clean-up and annuals')
+mainCtrl.toDoItem2 = new toDoItem('John',  'Jones',    'Watering only')
+mainCtrl.toDoItem3 = new toDoItem('Alan',  'Anderson', 'Irrigation and watering')
 
 //==Clients Array==\\
 mainCtrl.clientsArray = [mainCtrl.Smith, mainCtrl.Jones, mainCtrl.Anderson]
 
 //==To-Dos Array==\\
 mainCtrl.toDosArray = [mainCtrl.toDoItem1, mainCtrl.toDoItem2, mainCtrl.toDoItem3]
+var toDosAlpha = toDosArray.OrderBy(person => person.LastName);
 
 //=================================================\\
 //==============Controller Functions===============\\
 //=================================================\\
 mainCtrl.getClient = function(){
   mainCtrl.client = mainCtrl.clientsArray[$stateParams.index]
-  //indexOf , splice, filter "look up array methods"
-  console.log(mainCtrl.client)
 }
 
 
 //=================================================\\
 //============Client Constructor Function==========\\
 //=================================================\\
-function Client(name, phoneNumber, email, address, city, state, zipCode) {
-this.name = name
+function Client(firstName, lastName, phoneNumber, email, address, city, state, zipCode) {
+this.firstName = firstName
+this.lastName = lastName
 this.phoneNumber = phoneNumber
 this.email = email
 this.address = address
@@ -52,8 +52,9 @@ this.zipCode = zipCode
 //=================================================\\
 //============To-Do Constructor Function==========\\
 //=================================================\\
-function toDoItem(name, description){
-this.name = name
+function toDoItem(firstName, lastName, description){
+this.firstName = firstName
+this.lastName = lastName
 this.description = description
 }
 
