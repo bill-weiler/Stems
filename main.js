484c613cdc4fe6e5c64f1ff4fe4f090c664c5fef
Student <Student@Mac-mini-2.local>
2016-03-24 14:43:53 -0600

    responsiveness complete

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ function MainController($stateParams, $location) {
 //==============================================\\
 
 //==Clients==\\
-mainCtrl.Smith        = new Client('Sarah',  'Smith',     '555-555-5555',  'ssmith@gmail.com',    '123 ABC Rd.', 'Denver', 'CO', '80218')
-mainCtrl.Jones        = new Client('John',   'Jones' ,    '444-444-4444',  'jjones@gmail.com',    '456 DEF Rd.', 'Denver', 'CO', '80218')
-mainCtrl.Anderson     = new Client('Alan',   'Anderson' , '333-333-3333',  'aanderson@gmail.com', '789 GHI Rd.', 'Denver', 'CO', '80218')
+mainCtrl.Smith        = new Client('Sarah',  'Smith',     '555-555-5555',  'ssmith@gmail.com',    '123 ABC Rd.', 'Denver', 'CO', '80218', '98765')
+mainCtrl.Jones        = new Client('John',   'Jones' ,    '444-444-4444',  'jjones@gmail.com',    '456 DEF Rd.', 'Denver', 'CO', '80218', '54321')
+mainCtrl.Anderson     = new Client('Alan',   'Anderson' , '333-333-3333',  'aanderson@gmail.com', '789 GHI Rd.', 'Denver', 'CO', '80218', '45678')
 mainCtrl.clientsArray = [mainCtrl.Smith, mainCtrl.Jones, mainCtrl.Anderson]
 mainCtrl.newClient    = {}
 
@@ -26,8 +26,9 @@ mainCtrl.toDosArray  = [mainCtrl.toDoItem1, mainCtrl.toDoItem2, mainCtrl.toDoIte
 mainCtrl.newToDoItem = ''
 
 //==Property Notes==\\
-mainCtrl.propertyNote1   = new PropertyNote('Does not like rose bushes')
-mainCtrl.PropertyNote    = ''
+// mainCtrl.propertyNote1   = new PropertyNote('Does not like rose bushes')
+mainCtrl.newPropertyNote   = ''
+mainCtrl.propertyNoteArray = []
 
 //=================================================\\
 //==============Controller Functions===============\\
@@ -69,40 +70,35 @@ mainCtrl.removeClient = function() {
   }
 }
 
-// mainCtrl.addNote = function(){
-//   var myPropNote = new PropertyNote(propNote)
-//   mainCtrl.client.push(myPropNote)
-//   mainCtrl.newToDoItem = ''
-// }
+mainCtrl.addNote = function(){
+  mainCtrl.client.notes.push(mainCtrl.newPropertyNote)
+  mainCtrl.newPropertyNote = ''
+}
 
 //=================================================\\
 //============Client Constructor Function==========\\
 //=================================================\\
-function Client(firstName, lastName, phoneNumber, email, address, city, state, zipCode) {
-this.firstName = firstName
-this.lastName = lastName
+function Client(firstName, lastName, phoneNumber, email, address, city, state, zipCode, doorCode) {
+this.firstName   = firstName
+this.lastName    = lastName
 this.phoneNumber = phoneNumber
-this.email = email
-this.address = address
-this.city = city
-this.state = state
-this.zipCode = zipCode
+this.email       = email
+this.address     = address
+this.city        = city
+this.state       = state
+this.zipCode     = zipCode
+this.doorCode    = doorCode
+this.notes       = []
 }
 
 //=================================================\\
 //============To-Do Constructor Function==========\\
 //=================================================\\
 function ToDoItem(firstName, lastName, description){
-this.firstName = firstName
-this.lastName = lastName
+this.firstName   = firstName
+this.lastName    = lastName
 this.description = description
 }
 
-//=================================================\\
-//========Property Note Constructor Function=======\\
-//=================================================\\
-function PropertyNote(propNote){
-this.propNote = propNote
-}
 
 } //End of controller
